0e23830fd66ca16b0f8a08a9fe0cfc3d7142fdbe
Jacob Tomaw <jacob.tomaw@gmail.com>
2017-04-06 10:40:55 -0500

    Keyboard settings

---
+++ script/app_settings.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+echo "#### APP SETTINGS BEGINNING #####"
+
+####################
+# Login items
+####################
+
+#osascript -e 'tell application "System Events" to make login item at end with properties {path:"/path/to/itemname", hidden:false}' > ~/.dotfiles/backup/list_loginitems.txt
+## Add items (commented but left for future use)
+## osascript -e 'tell application "System Events" to make login item at end with properties {path:"/path/to/itemname", hidden:false}'
+## Delete items (commented but left for future use)
+## osascript -e 'tell application "System Events" to delete login item "itemname"'
+#
+#osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/BeardedSpice.app", hidden:false}'
+#osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Caffeine.app", hidden:false}'
+#osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Dropbox.app", hidden:false}'
+#osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Flux.app", hidden:false}'
+
+
+####################
+# Karabiner-Elements
+####################
+
+mkdir -p ~/.config/karabiner
+cat > ~/.config/karabiner/karabiner.json << EOF
+{
+    "global": {
+        "check_for_updates_on_startup": true,
+        "show_in_menu_bar": true,
+        "show_profile_name_in_menu_bar": false
+    },
+    "profiles": [
+        {
+            "name": "Default profile",
+            "selected": true,
+            "simple_modifications": {
+                "caps_lock": "left_control"
+            }
+        }
+    ]
+}
+EOF
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Karabiner-Elements", hidden:false}'
