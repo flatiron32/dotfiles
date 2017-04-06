47f6ab11bed3412d45bb88a640bd9dd45970165f
Jacob Tomaw <jacob.tomaw@gmail.com>
2017-04-06 09:32:58 -0500

    Initial bash links

---
+++ link/bash_alias.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+########################################
+####     ALIAS
+########################################
+
+#### OS ALIASES ####
+alias ll='ls -lGh $@'
+alias p4='ping 4.2.2.2'
+# Addressed in bashrch.sh, as this doesn't load properly
+# source ~/.dotfiles/private/*repos*.sh
+alias repofresh='~/.dotfiles/script/refresh_repos.sh'
+alias resource='source ~/.bash_profile'
+
+function enable_ntfs() {
+    VOLUME_NAME=$1
+    VOLUME_UUID=`diskutil info \`diskutil list|grep -e "${VOLUME_NAME}"|awk '{print $NF}'\`|grep UUID|awk '{print $NF}'`
+    FSTAB_STR="UUID=${VOLUME_UUID} none ntfs rw,auto,nobrowse"
+    sudo cat ${FSTAB_STR} > /etc/fstab
+    diskutil umount "${VOLUME_NAME}"
+    diskutil mount "${VOLUME_NAME}"
+}
+
+#### GIT ALIASES ####
+alias gs='git status'
+alias gsd='echo; git branch; echo; git status; echo; git log -1; echo; echo "CURRENT TIME: `date`"; echo'
+alias gbc="git status | awk '{print \$3}' | head -n 1 | pbcopy"
+alias gboth='git checkout master && git pull origin master && git checkout develop && git pull origin develop'
+
+#### APP ALIASES ####
---
+++ link/bash_profile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+########################################
+####     SOURCE
+########################################
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+    fi
+fi
