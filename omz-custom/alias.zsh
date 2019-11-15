30f4b65a1d5ac4e8d6b727a447e9dee8b551e3d6
Jacob Tomaw <jtomaw@shoprunner.com>
2019-11-15 12:30:39 -0500

    Adding aliases

---
+++ omz-custom/alias.zsh
@@ -0,0 +1,31 @@
+alias resource='source ~/.zshrc'
+
+##### GIT ALIASES #####
+alias gs='git status'
+alias gsd='echo; ~/.git-branch-status -va; echo; git status; echo; git --no-pager log -1; echo; echo "CURRENT TIME: `date`"; echo'
+alias gbc="git status | awk '{print \$3}' | head -n 1 | pbcopy"
+
+function local_git_check() {
+  for repo in $(locate .git | grep git$ | grep -v "Homebrew" | sed 's/\/.git//g')
+  do
+    cd $repo
+    git pull --all
+    res=$(gsd)
+    if [[ $res != *up-to-date* ]] || [[ $res == *"Untracked files"* ]] || [[ $res == *"Changes not staged"*  ]]
+    then
+      gsd
+      break
+    fi
+  done
+}
+
+function remaster() {
+  BRANCH=$(git status | awk '{print $3}' | head -n 1)
+  git checkout master
+  git pull --prune
+  git checkout ${BRANCH}
+  git rebase $1 master
+}
+
+#### APP ALIASES ####
+alias f='fuck'
